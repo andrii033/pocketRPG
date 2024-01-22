72898617f7eb91578ba3f20ee57ce68d142f6c99
andrii <ataran033@gmail.com>
2024-01-22 18:42:39 +0100

    initial commit

--- src/main/java/com/ta/pocketRPG/model/GameCharacter.java
+++ src/main/java/com/ta/pocketRPG/model/GameCharacter.java
@@ -10,6 +10,7 @@ public class GameCharacter {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String characterName;
     private int lvl=1;
     private int resets;
