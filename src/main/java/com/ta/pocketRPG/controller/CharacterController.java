ae5e20bb4820c68400b0ea8848c7042fc9b4d129
andrii <ataran033@gmail.com>
2024-03-25 21:04:33 +0100

    correct method fight

--- src/main/java/com/ta/pocketRPG/controller/CharacterController.java
+++ src/main/java/com/ta/pocketRPG/controller/CharacterController.java
@@ -1,5 +1,6 @@
 package com.ta.pocketRPG.controller;
 
+import com.ta.pocketRPG.component.RequestRateLimiter;
 import com.ta.pocketRPG.domain.dto.CharacterRequest;
 import com.ta.pocketRPG.domain.dto.FightRequest;
 import com.ta.pocketRPG.domain.model.Enemy;
@@ -9,6 +10,8 @@ import com.ta.pocketRPG.service.CharacterService;
 import com.ta.pocketRPG.service.EnemyService;
 import com.ta.pocketRPG.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,23 +30,56 @@ public class CharacterController {
     private UserService userService;
     @Autowired
     private EnemyService enemyService;
+    @Autowired
+    private RequestRateLimiter rateLimiter;
 
     @PostMapping("/create")
-    public String createCharacter(@RequestBody CharacterRequest characterRequest){
+    public String createCharacter(@RequestBody CharacterRequest characterRequest) {
         characterService.createCharacter(characterRequest);
         return "create";
     }
 
+    //    @PostMapping("/fight")
+//    public FightRequest fight(@RequestBody FightRequest fightRequest){
+//        if (rateLimiter.allowRequest()) {
+//            GameCharacter gameCharacter=characterRepository.findGameCharacterByUser(userService.getCurrentUser());
+//            Enemy enemy= enemyService.findEnemyById((long) gameCharacter.getEnemyId());
+//            fightRequest.setCharacterName(gameCharacter.getCharacterName());
+//            fightRequest.setCharacterLatestDam(gameCharacter.getLatestDam());
+//            fightRequest.setCharacterHp(gameCharacter.getHp());
+//            fightRequest.setEnemyName(enemy.getName());
+//            fightRequest.setEnemyHp(enemy.getHp());
+//            fightRequest.setEnemyLatestDam(enemy.getLatestDam());
+//            return fightRequest;
+//        } else {
+//            // Return an error response or handle the rate limit exceeded scenario
+//            return fightRequest;
+//        }
+//    }
     @PostMapping("/fight")
-    public FightRequest fight(@RequestBody FightRequest fightRequest){
-        GameCharacter gameCharacter=characterRepository.findGameCharacterByUser(userService.getCurrentUser());
-        Enemy enemy= enemyService.findEnemyById((long) gameCharacter.getEnemyId());
-        fightRequest.setCharacterName(gameCharacter.getCharacterName());
-        fightRequest.setCharacterLatestDam(gameCharacter.getLatestDam());
-        fightRequest.setCharacterHp(gameCharacter.getHp());
-        fightRequest.setEnemyName(enemy.getName());
-        fightRequest.setEnemyHp(enemy.getHp());
-        fightRequest.setEnemyLatestDam(enemy.getLatestDam());
-        return fightRequest;
+    public ResponseEntity<?> fight(@RequestBody FightRequest fightRequest) {
+        if (rateLimiter.allowRequest()) {
+            GameCharacter gameCharacter = characterRepository.findGameCharacterByUser(userService.getCurrentUser());
+            if (gameCharacter != null && gameCharacter.getEnemyId() != 0) {
+                Enemy enemy = enemyService.findEnemyById((long) gameCharacter.getEnemyId());
+                if (enemy != null) {
+                    fightRequest.setCharacterName(gameCharacter.getCharacterName());
+                    fightRequest.setCharacterLatestDam(gameCharacter.getLatestDam());
+                    fightRequest.setCharacterHp(gameCharacter.getHp());
+                    fightRequest.setEnemyName(enemy.getName());
+                    fightRequest.setEnemyHp(enemy.getHp());
+                    fightRequest.setEnemyLatestDam(enemy.getLatestDam());
+                    return ResponseEntity.ok(fightRequest);
+                } else {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Enemy not found");
+                }
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No enemy assigned to the character");
+            }
+        } else {
+            // Handle the rate limit exceeded scenario
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Rate limit exceeded");
+        }
     }
+
 }
