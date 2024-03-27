529d466409e73da54c239e886fdca6253bb18414
andrii <ataran033@gmail.com>
2024-03-27 20:06:40 +0100

    initial instruction

--- src/main/java/com/ta/pocketRPG/domain/model/User.java
+++ src/main/java/com/ta/pocketRPG/domain/model/User.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Table(name = "users")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "username", unique = true, nullable = false)
