847718a36e16e8aa36929c4416e2dd62989875cc
andrii <ataran033@gmail.com>
2024-05-03 14:05:21 +0200

    does not throw an exception when such a user already exists

--- src/main/java/com/ta/pocketRPG/controller/AuthController.java
+++ src/main/java/com/ta/pocketRPG/controller/AuthController.java
@@ -8,6 +8,7 @@ import com.ta.pocketRPG.domain.dto.SignInRequest;
 import com.ta.pocketRPG.domain.dto.SignUpRequest;
 import com.ta.pocketRPG.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
